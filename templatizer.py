b346673ef6b409e5cca397b86604df7318157441
icebreaker <theicebreaker007@gmail.com>
2011-07-31 08:47:14 +0300

    added GPLv3 header comment

--- templatizer.py
+++ templatizer.py
@@ -1,5 +1,26 @@
 #!/usr/bin/env python
 
+"""
+	Copyright (c) 2011, Mihail Szabolcs
+
+	All rights reserved.
+
+	This file is part of Templatizer.
+
+	Templatizer is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Templatizer is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Templatizer.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 __author__		= 'Mihail Szabolcs'
 __description__ = 'General purpose data-driven generator.'
 __version__		= (0, 2, 0)
